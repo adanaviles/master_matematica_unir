a7a9995c5ad126f272d898006291fec20f398133
Adan <adan.aviles.ext@rsi.cajarural.com>
2022-01-17 17:55:49 +0100

    empiezo funciones

--- programacion_cientifica/practica_grupal/funciones_auxiliares.py
+++ programacion_cientifica/practica_grupal/funciones_auxiliares.py
@@ -1,3 +1,33 @@
-def funcion_mia(x):
-    valor = 2*x
-    return valor
+###############################################################################
+##                              Import packages                              ##
+###############################################################################
+
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+###############################################################################
+##                   Definicion de funciones  para ejercicios                ##
+###############################################################################
+
+# Seno
+sin_vec  = np.vectorize(math.sin)
+
+#Funcion de runge
+def runge_func(x):
+    value = 1/(1+25*x**2)
+    return value
+
+runge_vec = np.vectorize(runge_func)
+
+# Función exponencial
+def exp_func(x):
+    value = math.exp(-20*x**2)
+    return value
+
+exp_vec = np.vectorize(exp_func)
+
+
+###############################################################################
+##                   Definicion de funciones  auxiliares                     ##
+###############################################################################
