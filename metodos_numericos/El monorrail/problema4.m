b822d0bca50f1d32fee27263db27c58d02789445
adanaviles <adan.aviles.cahill@gmail.com>
2022-03-12 19:57:08 +0100

    matlab

---
+++ metodos_numericos/El monorrail/problema4.m
@@ -0,0 +1,98 @@
+% Definimos primero xi e fi y x como variable simbolica.
+xi = [0 1.5 3 4 6 7 9 10];
+fi = [20 100 60 120 20 40 100 0];
+syms x
+% Calculamos b y el polinomio p 
+[ai,bi,ci,di,p] = splineCubicoNatural(xi,fi);
+
+% Tomamos 100 puntos para representar el polinomio
+xaxis_1 = linspace(xi(1),xi(2));xaxis_2 = linspace(xi(2),xi(3));
+xaxis_3 = linspace(xi(3),xi(4));xaxis_4 = linspace(xi(4),xi(5));
+xaxis_5 = linspace(xi(5),xi(6));xaxis_6 = linspace(xi(6),xi(7));
+xaxis_7 = linspace(xi(7),xi(8));
+% Definimos los y1,y2... para graficar.
+yaxis_1 = double(subs(p(1), x, xaxis_1));
+yaxis_2 = double(subs(p(2), x, xaxis_2));
+yaxis_3 = double(subs(p(3), x, xaxis_3));
+yaxis_4 = double(subs(p(4), x, xaxis_4));
+yaxis_5 = double(subs(p(5), x, xaxis_5));
+yaxis_6 = double(subs(p(6), x, xaxis_6));
+yaxis_7 = double(subs(p(7), x, xaxis_7));
+
+plot(xi,fi,'o');
+hold on;
+plot(xaxis_1,yaxis_1);
+plot(xaxis_2,yaxis_2);
+plot(xaxis_3,yaxis_3);
+plot(xaxis_4,yaxis_4);
+plot(xaxis_5,yaxis_5);
+plot(xaxis_6,yaxis_6);
+plot(xaxis_7,yaxis_7);
+hold off;
+
+function [ai,bi,ci,di,p] = splineCubicoNatural(xi,fi)
+    format shortg
+    n = length(xi);
+    h = zeros(1,n-1);
+    for i = 1:n-1
+        h(i) = xi(i+1)-xi(i);
+    end
+    ai  = fi;
+    %Relleno la diagonal inferior, superior y principal
+    %Relleno también b
+    dI = zeros(n-1,1);
+    dS = zeros(n-1,1);
+    dP = ones(n,1);
+    b = zeros(n,1);
+    for i = 2:n-1
+        dI(i-1) = h(i-1);
+        dS(i) = h(i);
+        dP(i) = 2*(h(i)+h(i-1));
+        b(i) = 3*((ai(i+1)-ai(i))/h(i) - (ai(i)-ai(i-1))/h(i-1));
+    end
+    % Aplico crout para sacar ci
+    ci  = Crout(dP ,dS ,dI ,b);
+    bi = zeros(n-1,1);
+    di = zeros(n-1,1);
+    % Obtengo bi y di
+    for i=1:n-1
+        bi(i) = (ai(i+1)-ai(i))/h(i) - h(i)*(2*ci(i)+ci(i+1))/3;
+        di(i) = (ci(i+1)-ci(i))/(3*h(i));
+    end
+    
+    syms x;
+    % En p tendré una lista con cada spline 
+    p = zeros(1,n);
+    syms p;
+    for i = 1:n-1
+        p(i) = ai(i)+bi(i)*(x-xi(i))+ci(i)*(x-xi(i))^2+di(i)*(x-xi(i))^3;
+    end
+end
+
+function x = Crout(dP ,dS ,dI ,b)
+    % La función x=Crout(dP,dS,dI,b) obtiene la ...
+    % solución del sistema Ax=b utilizando el ...
+    % algoritmo de Crout. dP, dS y dI son las ...
+    % diagonales principal , superior e inferior de A.
+    n = length(dP);
+    % 1. Obtención de las matrices L y U tales que A = LU
+    l(1) = dP(1);
+    u(1) = dS(1)/l(1);
+    for i =2:n-1
+        l(i) = dP(i)-dI(i-1)*u(i -1) ;
+        u(i) = dS(i)/l(i);
+    end
+    l(n) = dP(n)-dI(n-1)*u(n-1);
+    % 2. Solución del sistema Lz = b
+   
+    z(1) = b(1)/l(1) ;
+    for i = 2:n
+        z(i) = (1/l(i))*(b(i)-dI(i-1)*z(i-1));
+    end
+    % 3. Solución del sistema Ux = z
+    x(n) = z(n);
+    for i = n-1:-1:1
+        x(i) = z(i)-u(i)*x(i+1);
+    end
+    x=x(:);
+end
